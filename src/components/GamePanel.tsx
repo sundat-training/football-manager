aea71192ec9fe5f2503e2c6ea8c2f750cc8cf7c6
uninow <uninow@sundat.de>
2024-06-25 13:43:02 +0200

    feat: select actual game

--- src/components/GamePanel.tsx
+++ src/components/GamePanel.tsx
@@ -2,6 +2,7 @@ import { MannschaftsPanel } from './MannschaftsPanel';
 import { format } from 'date-fns';
 
 export type MatchDetails={
+  id: number,
   team1: string,
   team2: string,
   flag1: string,
@@ -13,16 +14,18 @@ export type MatchDetails={
 
 export type GamePanelProps ={
   matchDetails: MatchDetails,
-  date: Date
+  date: Date,
+  setGame: (id:number) => void
 }
 
-export function GamePanel({matchDetails,date}: GamePanelProps) {
+export function GamePanel({matchDetails,date,setGame}: GamePanelProps) {
   const {team1,score_team1,flag1,team2,score_team2,flag2,status} = matchDetails;
   const fdate= status=="finished" ? "Endstand" : format(date,"eeeeee, dd.MM")
   const ftime= status=="finished" ? format(date,"eeeeee, dd.MM"): format(date,"HH:mm")
   return (
     <div
       className='flex flex-row gap-x-2 p-4 border shadow-md'
+       onClick={() => setGame(matchDetails.id)      }
     >
       <div
         className='w-3/4 flex flex-col space-y-1 justify-center'
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,17 +1,6 @@
 import './App.css'
 import { GamePanel, MatchDetails } from '../components/GamePanel'
 import { useEffect, useState } from 'react';
-const zdata:MatchDetails=  {
-    team1: "Deutschland",
-    score_team1: 5,
-    flag1: "deu",
-    team2: "Scottland",
-    score_team2: 1,
-    flag2: "sct",
-    status: "finished"
-  }
-const gdata= [1,2,3,4].map(i => ({...zdata}))
-console.log(gdata);
  
 const api = "http://localhost:3000"
 
@@ -20,13 +9,14 @@ type Group = 'A' | 'B' | 'C' | 'D' | 'E' | 'F';
 type GameStatus = 'finished' | 'running' | 'pending';
 
 interface Game {
+  id: number;
   group: Group;
-  datetime: string; // ISO 8601 format with timezone
+  datetime: Date; // ISO 8601 format with timezone
   team1: number;
   team2: number;
   status: GameStatus;
-  score_team1: number | null;
-  score_team2: number | null;
+  score_team1: number ;
+  score_team2: number ;
 }
 interface Team {
   id: number;
@@ -38,67 +28,83 @@ interface Team {
 
 function App() {
   const [teams,setTeams]= useState([])
-  const [games,setGames]= useState([])
-  const POLLING_INTERVAL = 2000
+  const [games,setGames]= useState<Game[]>([])
+  const [actGame, setActGame] = useState<Game>()
 
-  const gameData: MatchDetails={
-    team1: "Deutschland",
-    score_team1: 5,
-    flag1: "deu",
-    team2: "Scottland",
-    score_team2: 1,
-    flag2: "sct",
-    status: "finished"
-  }
-  const date= new Date("2024.06.24 18:30")
+  const POLLING_INTERVAL = 2000
+  
   
   useEffect( () =>{
    
-        const intervalId = setInterval(() => {
+    const intervalId = setInterval(() => {
       console.log("moep");
-      fetch(api+ "/teams")
+      fetch(api + "/teams")
         .then(b => b.json())
         .then(data => setTeams(data));
-  
-      fetch(api+ "/games")
+
+      fetch(api + "/games")
         .then(b => b.json())
-        .then(data => setGames(data));
-        }, 
+        //@ts-ignore
+        .then(d => d.map(g => ({...g, dateTime:new Date(g.dateTime)})))
+        .then(data => setGames(data)
+                      
+                      );
         
-        
+    },
       POLLING_INTERVAL);
     
         return () => clearInterval(intervalId);
   }, [] )
 
+  useEffect( () =>{
+      // console.log(JSON.stringify(
+      //   games.map( (g:Game,idx) =>({...g,id:idx+1}))
+      //   ));
+  } , [games])
 
-  function joinTeams(g : Game, teams : Team[]){
-      const team1 = teams.filter(t => t.id == g.team1)[0].name
-      const team2 = teams.filter(t => t.id == g.team2)[0].name
-      return {...g, team1,team2} as unknown as MatchDetails
+  function handleChangeGame(id: number){
+    const gg = games.filter(g => g.id == id)[0]
+    if (gg) setActGame(gg)
+  }
+  
+  function joinTeams(g : Game, teams : Team[]) :MatchDetails{
+      const t1 = teams.filter(t => t.id == g.team1)[0]
+      const t2 = teams.filter(t => t.id == g.team2)[0]
+      
+      const team1=t1.name
+      const team2=t2.name
+
+      const flag1=t1.country_code
+      const flag2=t2.country_code
+      return {...g, team1,team2,flag1,flag2} 
   }
 
   return (
     <>
       
     <h1 className='mb-5'>Fußballmanager</h1>
-    <div>
+     <div>
       <div>#teams: {teams.length}</div>
       <div>#games: {games.length}</div>
     </div>
+    <div
+    className='mt-4'
+    >
 
-     <GamePanel matchDetails={gameData} date={date} /> 
-
+      {actGame  
+      ? <GamePanel matchDetails={joinTeams(actGame,teams)} date={actGame.datetime} setGame={_ => console.log("nope") }/> 
+      : <></> }
+    </div>
      <div
      className='mt-6  grid grid-cols-2 gap-2'
      >
       {
         games.map( g =>
-          <GamePanel matchDetails={joinTeams(g,teams)} date={date}/>
+          <GamePanel matchDetails={joinTeams(g,teams)} date={g.datetime} setGame={(id) => handleChangeGame(id) }/>
         )
       }
 
-     </div>
+     </div> 
     </>
   )
 }
