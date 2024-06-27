a8dca9521e69e850b825885dc4da7a2f9bd7115a
uninow <uninow@sundat.de>
2024-06-27 08:32:17 +0200

    rfct: reduce redundant parts in ScoreGamePanel

--- src/components/ScoreGamePanel.tsx
+++ src/components/ScoreGamePanel.tsx
@@ -1,40 +1,50 @@
 
-export function ScoreGamePanel() {
-  return (
+      export function ScoreGamePanel() {
+        return (
+          <div className='flex flex-row gap-x-2 p-2 border shadow-md'>
+            <ScoreTeam
+              team='Team A'
+              goal={0}
+              onMissGoal={() => {   }}
+              onScoreGoal={() => {   }}
+              onYellowCard={() => {   }}
+              onRedCard={() => {   }}
+            />
+            <ScoreTeam
+              team='Team B'
+              goal={0}
+              onMissGoal={() => {   }}
+              onScoreGoal={() => {   }}
+              onYellowCard={() => {   }}
+              onRedCard={() => {   }}
+            />
+          </div>
+        );
+      }
 
-    <div
-      className='flex flex-row gap-x-2 p-2 border shadow-md'
-    >
-      <div
-        className='w-1/2 flex flex-col space-y-1 justify-center'
-      >
-       <div>team1</div>
-       <div className="flex flex-row space-x-4">
-        <div>missGoal</div>
-        <div>Goal</div>
-       </div>
-       <div className="flex flex-row space-x-4">
-        <div>YC</div>
-        <div>RC</div>
-       </div>
+type ScoreTeamProps = {
+  team: string;
+  goal: number;
+  onMissGoal: () => void;
+  onScoreGoal: () => void;
+  onYellowCard: () => void;
+  onRedCard: () => void;
+};
 
-      </div>
-      <div
-        className='w-1/2 flex flex-col space-y-2 '
-      >
-       <div>team2</div>
+      function ScoreTeam({ team, goal, onMissGoal, onScoreGoal,  onYellowCard, onRedCard }: ScoreTeamProps) {
+        return (
+          <div className='w-1/2 flex flex-col space-y-1 justify-center'>
+            <div>{goal}</div>
+            <div>{team}</div>
+            <div className="flex flex-row space-x-4">
+              <button onClick={onMissGoal}>missGoal</button>
+              <button onClick={onScoreGoal}>goal</button>
+            </div>
+            <div className="flex flex-row space-x-4">
+              <button onClick={onYellowCard}>YC</button>
+              <button onClick={onRedCard}>RC</button>
+            </div>
+          </div>
+        );
+      }
 
-       <div className="flex flex-row space-x-4">
-        <div>missGoal</div>
-        <div>Goal</div>
-       </div>
-       <div className="flex flex-row space-x-4">
-        <div>YC</div>
-        <div>RC</div>
-       </div>
-
-      </div>
-
-    </div>
-  );
-}
